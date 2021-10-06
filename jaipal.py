778c804c62de1057eb2233a2a06ccc35b90aea4e
Jaipal17 <90744469+Jaipal17@users.noreply.github.com>
2021-10-06 17:26:05 +0530

    Add files via upload

---
+++ jaipal.py
@@ -0,0 +1,15 @@
+TASK 1:
+
+from math import pi
+r=float(input("Input the radius of the circle : "))
+area=pi*r*r
+print("the area of the circle with radius 1.1 is:",area)
+
+TASK 2:
+
+filename=input("Input the filename:")
+extension={
+    "py":"'python'",
+    "txt":"'text'"
+}
+print("The extension of the file is : ",extension["py"])
